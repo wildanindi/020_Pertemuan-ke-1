3b84691e44568de53da99c92a92c8ab2d11d0aad
wildanindi <wildanindi2@gmail.com>
2024-02-27 18:50:07 +0700

    Add project files.

---
+++ Pertemuan ke-1/Pertemuan ke-1.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+    
+    float nJejari, nLuas;
+    cout << "Masukkan Jejari = ";
+    cin >> nJejari;
+
+    nLuas = 3.14 * nJejari * nJejari;
+
+    cout << "Luas lingkaran adalah = " << nLuas << endl;
+
+    system("pause");
+}
